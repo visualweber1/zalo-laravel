1820d3fe3c0e995c9ff64e8082867f30ffb27ac2
visualweber1 <49316475+visualweber1@users.noreply.github.com>
2019-04-08 16:07:57 +0700

    Update ZaloProviderUser.php

--- src/ZaloProviderUser.php
+++ src/ZaloProviderUser.php
@@ -12,9 +12,9 @@ class ZaloProviderUser
 
 	public function __construct($providerUser)
 	{
-		$this->id = isset($providerUser["token"]["id"]) ? $providerUser["token"]["id"] : null;
-		$this->name = isset($providerUser["token"]["name"]) ? $providerUser["token"]["name"] : null;
-		$this->avatar_original = isset($providerUser["token"]["picture"]["data"]["url"]) ? $providerUser["token"]["picture"]["data"]["url"] : null; 
+		$this->id = isset($providerUser["id"]) ? $providerUser["id"] : null;
+		$this->name = isset($providerUser["name"]) ? $providerUser["name"] : null;
+		$this->avatar_original = isset($providerUser["picture"]["data"]["url"]) ? $providerUser["picture"]["data"]["url"] : null; 
 	}
 
 	public function getEmail()
